870b2ac49fea41eb403e28aaf73746d79baf6286
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2025-03-11 20:56:11 +0100

    chore: cleanup

--- src/ExpoModulesWorkshopsModule.ts
+++ src/ExpoModulesWorkshopsModule.ts
@@ -2,5 +2,4 @@ import { NativeModule, requireNativeModule } from 'expo';
 
 export declare class ExpoModulesWorkshopsModule extends NativeModule {}
 
-// This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoModulesWorkshopsModule>('ExpoModulesWorkshops');
